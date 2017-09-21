ca24a1cc42d17c2eb101add9e17f82a1ff89aebe
Samantha Troy <S.troy1220@gmail.com>
2017-09-21 18:01:49 -0400

    more classes

--- Capstone/Classes/ReadInInventory.cs
+++ Capstone/Classes/ReadInInventory.cs
@@ -5,9 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+
 namespace Capstone.Classes
 {
-    class ReadInInventory
+    public class ReadInInventory
     {
         private string filePath;
         private int Cost;
@@ -15,10 +16,87 @@ namespace Capstone.Classes
         private int Product;
         private int Slotld;
 
-        public ReadInInventory (string filePath)
+        public ReadInInventory(string filePath)
         {
+            filePath = @"C:\Users\Samantha Troy\Desktop\c-module-1-capstone-team-7\etc\vendingmachine.csv";
             this.filePath = filePath;
+
         }
 
+
+        //private List<ItemGeneral> CreateInitialInventory(string[] lineData)
+        //{
+            
+        //}
+
+
+        Dictionary<string, List<ItemGeneral>> inventory = new Dictionary<string, List<ItemGeneral>>();
+        
+
+        public Dictionary<string, List<ItemGeneral>> GetInventory()
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                while (!sr.EndOfStream)
+                {
+
+                    string thisLine = sr.ReadLine();
+                    string[] temp = thisLine.Split('|');
+                    List<ItemGeneral> list = new List<ItemGeneral>();
+
+
+                    if (temp[0].Contains("A"))
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            ChipsItem chips = new ChipsItem(temp[1], decimal.Parse(temp[2]));
+                            list.Add(chips);
+
+                        }
+
+                    }
+
+                    if (temp[0].Contains("B"))
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            CandyItem candy = new CandyItem(temp[1], decimal.Parse(temp[2]));
+                            list.Add(candy);
+
+                        }
+
+                    }
+
+                    if (temp[0].Contains("C"))
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            BeverageItem drink = new BeverageItem(temp[1], decimal.Parse(temp[2]));
+                            list.Add(drink);
+
+                        }
+
+                    }
+
+
+                    if (temp[0].Contains("D"))
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            GumItem gum = new GumItem(temp[1], decimal.Parse(temp[2]));
+                            list.Add(gum);
+
+                        }
+
+                    }
+
+
+                    inventory[temp[0]] = list;
+                    
+                }
+
+            }
+            return inventory;
+        }
     }
 }
---
+++ CapstoneTests/InventoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Capstone.Classes;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class InventoryTests
+    {
+        [TestMethod]
+        public void AmountOfInvItems()
+        {
+            ReadInInventory monkey = new ReadInInventory(@"C: \Users\Samantha Troy\Desktop\c - module - 1 - capstone - team - 7\etc\vendingmachine.csv");
+            //int inv =monkey.GetInventory();
+
+
+            Assert.AreEqual(16, monkey.GetInventory().Count);
+            
+        }
+    }
+}
