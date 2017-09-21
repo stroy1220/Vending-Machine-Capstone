bfcfc12d973d86d8e8af8ba25e62f351210651b1
Scott Miller <miller.4583@gmail.com>
2017-09-21 09:50:53 -0400

    adding candy class

---
+++ Capstone/Classes/CandyItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    class CandyItem
+    {
+        //private string name;
+        //private string slot;
+
+        public virtual double Cost
+        {
+            get { return 0; }
+        }
+
+        public virtual string Slot
+        {
+            get { return ""; }
+        }
+
+
+        public virtual string Name
+        {
+            get { return ""; }
+
+        }
+
+
+
+    }
+}
---
+++ Capstone/Classes/WonkaBar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    class WonkaBar : CandyItem
+    {
+        public override double Cost => 1.25;
+        public override string Name => "Wonka Bar";
+        public override string Slot => "A1";
+    }
+}
