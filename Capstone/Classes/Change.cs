b899ca44bc03fdc7795b4ae56b07e35575c29f27
Scott Miller <miller.4583@gmail.com>
2017-09-21 15:59:59 -0400

    adding change&Test also begining of readinventory

---
+++ Capstone/Classes/Change.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Classes
+{
+    public class Change
+    {
+        private int dimes;
+        private int nickles;
+        private int quarters;
+        private double totalChange;
+
+
+        public int Dimes
+        {
+            get { return this.dimes; }
+        }
+        public int Nickels
+        {
+            get { return this.nickles; }
+        }
+        public int Quarters
+        {
+            get { return this.quarters; }
+        }
+        public double TotalChange
+        {
+            get { return this.totalChange; }
+        }
+        public Change(decimal amountInDollars)
+        {
+            this.totalChange = decimal.ToDouble(amountInDollars);
+            while (amountInDollars >= .05M)
+            {
+                if (amountInDollars > .25M)
+                {
+                    this.quarters++;
+                    amountInDollars -= .25M;
+
+                }
+                else if(amountInDollars > .10M)
+                {
+                    this.dimes++;
+                    amountInDollars -= .10M;
+                }
+                else if(amountInDollars == .05M)
+                {
+                    this.nickles++;
+                    amountInDollars -= .05M;
+                }
+
+              
+                
+            }
+             
+        }
+        public Change(int amountInCents)
+        {
+            this.quarters = amountInCents / 25;
+            this.dimes = amountInCents / 10;
+            this.nickles = amountInCents / 5;
+
+
+        }
+        public bool Equals(object obj)
+        {
+            return (TotalChange == 0);
+        }
+
+
+
+    }
+}
---
+++ Capstone/Classes/ReadInInventory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Capstone.Classes
+{
+    class ReadInInventory
+    {
+        private string filePath;
+        private int Cost;
+        private int InitialQuantity;
+        private int Product;
+        private int Slotld;
+
+        public ReadInInventory (string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+    }
+}
